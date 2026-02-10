82fad555ed651003e3d1644ea3bac84a2dab757e
Win Win <adwin.imperial@gmail.com>
2026-02-10 20:36:05 +0000

    File changes

--- src/pages/Teams.jsx
+++ src/pages/Teams.jsx
@@ -165,8 +165,8 @@ export default function TeamsPage() {
           <Button 
             onClick={() => setShowCreateDialog(true)}
             className="bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 text-white shadow-lg shadow-blue-500/30 h-12 px-6"
-            disabled={leagues.length === 0}
-          >
+            disabled={assignedLeagues.length === 0}
+            >
             <Plus className="w-5 h-5 mr-2" />
             Add Team
           </Button>
