7ddfdec69d3d2b72d9c7f7c016f53214ed77442a
Win Win <adwin.imperial@gmail.com>
2026-02-10 15:54:13 +0000

    File changes

--- src/components/hooks/use-mobile.jsx
+++ src/components/hooks/use-mobile.jsx
@@ -6,13 +6,13 @@ export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState(undefined)
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
+    const mq = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
     const onChange = () => {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     }
-    mql.addEventListener("change", onChange)
+    mq.addEventListener("change", onChange)
     setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
+    return () => mq.removeEventListener("change", onChange)
   }, [])
 
   return !!isMobile
