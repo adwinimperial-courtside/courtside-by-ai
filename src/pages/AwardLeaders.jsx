477344557f48884bb36e2c795a752bb09c469787
Win Win <adwin.imperial@gmail.com>
2026-02-10 20:41:05 +0000

    File changes

--- src/pages/AwardLeaders.jsx
+++ src/pages/AwardLeaders.jsx
@@ -81,13 +81,13 @@ export default function AwardLeadersPage() {
                 <SelectValue placeholder="Select league" />
               </SelectTrigger>
               <SelectContent>
-                <SelectItem value="all">All Leagues</SelectItem>
-                {leagues.map(league => (
-                  <SelectItem key={league.id} value={league.id}>
-                    {league.name} ({league.season})
-                  </SelectItem>
-                ))}
-              </SelectContent>
+                 <SelectItem value="all">All Leagues</SelectItem>
+                 {assignedLeagues.map(league => (
+                   <SelectItem key={league.id} value={league.id}>
+                     {league.name} ({league.season})
+                   </SelectItem>
+                 ))}
+               </SelectContent>
             </Select>
           </div>
         </div>
