3d664e5b74760ae575951919cdfb48439710af69
Win Win <adwin.imperial@gmail.com>
2026-03-10 22:09:51 +0000

    File changes

--- src/components/live/LiveStatTracker.jsx
+++ src/components/live/LiveStatTracker.jsx
@@ -1240,7 +1240,7 @@ export default function LiveStatTracker({ game, homeTeam, awayTeam, players, exi
         </div>
 
         <div className="flex-shrink-0 mb-2">
-          <ScoreHeader game={game} homeTeam={homeTeam} awayTeam={awayTeam} onGameUpdate={onGameUpdate} onEndGame={handleEndGameFromModal} lineupBlocked={!!repairMode} />
+          <ScoreHeader game={liveGame} homeTeam={homeTeam} awayTeam={awayTeam} onGameUpdate={onGameUpdate} onEndGame={handleEndGameFromModal} lineupBlocked={!!repairMode} />
         </div>
 
         <div className="flex gap-3 flex-1 min-h-0">
