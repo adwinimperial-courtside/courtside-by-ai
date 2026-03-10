07191a7dbae41a1ebf1635caa102a24d5a00eaab
Win Win <adwin.imperial@gmail.com>
2026-03-10 22:29:43 +0000

    File changes

--- src/pages/LiveBoxScore.jsx
+++ src/pages/LiveBoxScore.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { base44 } from "@/api/base44Client";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
