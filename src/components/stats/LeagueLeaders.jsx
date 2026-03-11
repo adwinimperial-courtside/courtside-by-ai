5df095b7e228afcdf92b56cdbf56bf59790a5ab7
Win Win <adwin.imperial@gmail.com>
2026-03-11 17:02:55 +0000

    File changes

--- src/components/stats/LeagueLeaders.jsx
+++ src/components/stats/LeagueLeaders.jsx
@@ -4,14 +4,15 @@ import { Award } from "lucide-react";
 
 export default function LeagueLeaders({ players, teams, stats }) {
   const didPlayerParticipate = (stat) => {
-    if (stat.did_play) return true;
-    if (stat.is_starter) return true;
-    if ((stat.minutes_played || 0) > 0) return true;
     const hasStats = (stat.points_2 || 0) + (stat.points_3 || 0) + (stat.free_throws || 0) +
                      (stat.assists || 0) + (stat.steals || 0) + (stat.blocks || 0) +
                      (stat.offensive_rebounds || 0) + (stat.defensive_rebounds || 0) +
                      (stat.fouls || 0) + (stat.technical_fouls || 0) + (stat.unsportsmanlike_fouls || 0) > 0;
-    return hasStats;
+    
+    if (stat.did_play) return true;
+    if ((stat.minutes_played || 0) > 0) return true;
+    if (hasStats) return true;
+    return false;
   };
 
   const playerAggregates = players.map(player => {
