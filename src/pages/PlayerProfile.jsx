cd4a71d6135a0bf4e468b0e0463a36ad6238793c
Win Win <adwin.imperial@gmail.com>
2026-03-11 14:25:48 +0000

    File changes

--- src/pages/PlayerProfile.jsx
+++ src/pages/PlayerProfile.jsx
@@ -164,6 +164,7 @@ export default function PlayerProfile() {
             allStats={allCompletedStats}
             games={leagueGames}
             teamId={teamId}
+            leagueId={selectedLeagueId}
             leagueName={selectedLeague?.name}
             onPhotoUpdate={handlePhotoUpdate}
           />
